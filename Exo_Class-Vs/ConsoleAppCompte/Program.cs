3d3e3d00ac5d846e2e8d14e2a66fead5c32fdc16
Mathieu AKNOUCHE <MAknouche@arfp.asso.fr>
2023-01-06 11:53:43 +0100

    ajout exo compteBancaire

---
+++ Exo_Class-Vs/ConsoleAppCompte/Program.cs
@@ -0,0 +1,21 @@
+﻿using LibrairieCompte;
+
+namespace ConsoleAppCompte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            CompteBancaire monCompte;
+            CompteBancaire autreCompte;
+            
+            monCompte = new CompteBancaire("FR_lecompteAmoi","Akn","math",938.38f,1500);
+            autreCompte = new CompteBancaire("FR-leAlui", "Mr", "truc", 500.01f, 1500);
+
+            bool resultat = monCompte.Superieur(autreCompte);
+
+
+        }
+    }
+}
