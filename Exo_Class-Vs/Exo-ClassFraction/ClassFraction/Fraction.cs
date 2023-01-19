0939154daa96f0022b16737b1c364e33b1bac40e
Mathieu AKNOUCHE <MAknouche@arfp.asso.fr>
2023-01-19 16:54:50 +0100

    ajout exo fraction et ECF1

---
+++ Exo_Class-Vs/Exo-ClassFraction/ClassFraction/Fraction.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassFraction
+{
+    public class Fraction
+    {
+        public int numerateur;
+        public int denominateur;
+
+        public Fraction(int _numerateur, int _denominateur)
+        {
+            numerateur = _numerateur;
+            denominateur = _denominateur;
+            
+        }
+
+        public Fraction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Fraction(int _numerateur)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Fraction(Fraction _fraction)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return string.Format(base.ToString() + " {0} / {1}", numerateur, denominateur);
+        }
+    }
+}
---
+++ Exo_Class-Vs/Exo-ClassFraction/ConsoleFraction/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassFraction;
+
+namespace ConsoleFraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Fraction f1 = new Fraction(12, 7);
+           Console.WriteLine(f1.ToString());
+        }
+    }
+}
