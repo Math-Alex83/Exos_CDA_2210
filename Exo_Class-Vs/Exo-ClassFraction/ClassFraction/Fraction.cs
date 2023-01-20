2db37da9c6905f6cbe8876d36c03bff8d42296bf
Mathieu AKNOUCHE <MAknouche@arfp.asso.fr>
2023-01-20 11:54:26 +0100

    suite Fraction

--- Exo_Class-Vs/Exo-ClassFraction/ClassFraction/Fraction.cs
+++ Exo_Class-Vs/Exo-ClassFraction/ClassFraction/Fraction.cs
@@ -2,34 +2,73 @@
 {
     public class Fraction
     {
-        public int numerateur;
-        public int denominateur;
+        private int numerateur;
+        private int denominateur;
 
+        public int Denominateur { get => denominateur; }
+        //set => denominateur = value; }
+        public int Numerateur { get => numerateur; }
+        //set => numerateur = value; }
+
+        /// <summary>
+        /// Constructeur avec paramètres.
+        /// </summary>
+        /// <param name="_numerateur"></param>
+        /// <param name="_denominateur"></param>
         public Fraction(int _numerateur, int _denominateur)
         {
-            numerateur = _numerateur;
-            denominateur = _denominateur;
+            this.numerateur = _numerateur;
+            this.denominateur = _denominateur;
             
         }
 
-        public Fraction()
-        {
-            throw new System.NotImplementedException();
-        }
+        /// <summary>
+        /// Constructeur par défault.
+        /// </summary>
+        public Fraction() : this(0, 1) { }
 
+        /// <summary>
+        /// Contructeur avec numérateur en paramètre.
+        /// </summary>
+        /// <param name="_numerateur"></param>
         public Fraction(int _numerateur)
         {
-            throw new System.NotImplementedException();
+            numerateur = _numerateur;
+            denominateur = 1;
         }
 
-        public Fraction(Fraction _fraction)
-        {
-            throw new System.NotImplementedException();
-        }
-
+        /// <summary>
+        /// Constructeur par recopie.
+        /// </summary>
+        /// <param name="_fraction"></param>
+        public Fraction(Fraction _fraction) : 
+            this(_fraction.numerateur, _fraction.denominateur) 
+         { }
+        
+        /// <summary>
+        /// Affichage 
+        /// </summary>
+        /// <returns>string</returns>
         public override string ToString()
         {
-            return string.Format(base.ToString() + " {0} / {1}", numerateur, denominateur);
+            return string.Format(base.ToString() + " {0} / {1}", Numerateur, Denominateur);
+        }
+
+        // numérateur & dénominateur (int) resultat (double)! 
+
+        // 1) « 0 » pour la fraction 0/1.
+
+        public string NumerateurNull(int _numerateur)
+        {
+            if (_numerateur==0)
+            {
+                return string.Format(base.ToString() + " {0} ", Numerateur);
+            }
+            else
+            {
+                return string.Format(base.ToString() + " {0} / {1}", Numerateur, Denominateur);
+
+            }
         }
     }
 }
--- Exo_Class-Vs/Exo-ClassFraction/ConsoleFraction/Program.cs
+++ Exo_Class-Vs/Exo-ClassFraction/ConsoleFraction/Program.cs
@@ -1,13 +1,22 @@
 ﻿using ClassFraction;
-
 namespace ConsoleFraction
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           Fraction f1 = new Fraction(12, 7);
-           Console.WriteLine(f1.ToString());
+            // « 12/7 » pour la fraction 12/7
+            Fraction f1 = new Fraction(12, 7);
+            Console.WriteLine(f1.ToString());       // OK
+
+            // « 0 » pour la fraction 0/1
+            Fraction f2 = new Fraction();
+            Console.WriteLine(f2.ToString());       // 
+
+            // « 9 » pour la fraction 9/1
+            Fraction f3 = new Fraction(9);
+            Console.WriteLine(f3.ToString());       //
+
         }
     }
 }
