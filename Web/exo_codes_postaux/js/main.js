928cf0c484310b9c475b2dcc78d9cdd055c38c8c
Mathieu AKNOUCHE <mathalex68@gmx.fr>
2023-06-05 17:02:01 +0200

    debut web

---
+++ Web/exo_codes_postaux/js/main.js
@@ -0,0 +1,29 @@
+// // JavaScript = language prototype ! le prototype est un objet en lui même. 
+
+// const constante = ""; // La valeur doit être initialisée de suite pour une constante. d'où son nom...
+
+// // Différence entre let et var
+// var variable1; // a une porté de fonction
+// //exemple
+// function maFonction()
+// {
+//     if(true)
+//     {
+//         var toto;
+//     }
+
+//     console.log(toto);
+// }
+// // en dehors de la fonction, la variable var n'est plus accessible.
+
+// let variable2; // a une porté de bloc
+// // exemple2
+// function maFonction2()
+// {
+//     if(true)
+//     {
+//         let toto; // en dehors de ce BLOC {} de code la variable let n'est plus accessible.
+//     }
+
+//     console.log(toto);
+// }
