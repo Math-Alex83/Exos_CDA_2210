50c4dae6ade22c00f916e8c2dc4745afc6f63d90
Mathieu AKNOUCHE <mathalex68@gmx.fr>
2023-06-06 17:04:35 +0200

    exo ville

--- Web/exo_codes_postaux/js/main.js
+++ Web/exo_codes_postaux/js/main.js
@@ -1,9 +1,9 @@
 // // JavaScript = language prototype ! le prototype est un objet en lui même. 
 
-// const constante = ""; // La valeur doit être initialisée de suite pour une constante. d'où son nom...
+// const constante = ""; // La valeur doit être initialisée à la déclaration. d'où son nom...
 
 // // Différence entre let et var
-// var variable1; // a une porté de fonction
+// var variable1; // a une portée de fonction
 // //exemple
 // function maFonction()
 // {
@@ -16,7 +16,7 @@
 // }
 // // en dehors de la fonction, la variable var n'est plus accessible.
 
-// let variable2; // a une porté de bloc
+// let variable2; // a une portée de bloc
 // // exemple2
 // function maFonction2()
 // {
@@ -25,5 +25,35 @@
 //         let toto; // en dehors de ce BLOC {} de code la variable let n'est plus accessible.
 //     }
 
-//     console.log(toto);
+//     console.log(toto); ici la variable toto aura la valeur undefined / non défini
 // }
+
+
+
+const reponse = await fetch('https://arfp.github.io/tp/web/frontend/zipcodes/zipcodes.json');
+console.log(reponse);
+const json = await reponse.json();
+console.log(json);
+
+const saisie = document.getElementById('zipcodeaussi');
+const mesOptions = document.getElementById('mesOptions');
+const monBouton = document.getElementById('monBouton');
+
+for(let ville of json) 
+{
+   // console.log(ville.codePostal);
+
+    let option = document.createElement('option');
+    option.value = ville.codePostal;
+    mesOptions.appendChild(option)
+
+}
+
+monBouton.addEventListener('click', function() {
+    let valeur = saisie.value;
+    console.log(valeur);
+
+    let resultat = json.filter(ville => ville.codePostal == valeur);
+    console.log(resultat);
+    document.getElementById("divSuggestions").innerHTML = resultat.toString();
+})
