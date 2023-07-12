04626782d6dfc3af45ce02322f86b1c87b5f2c7c
Mathieu AKNOUCHE <mathalex68@gmx.fr>
2023-07-12 16:52:47 +0200

    DesignPattern/Expression

---
+++ Conception/03_Exos_CSharp/01_Expression/Expression/Calculer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression
+{
+    internal class Calculer
+    {
+        static double Additionner(double _valeurG, double _valeurD)
+        {
+            double resultat = _valeurG + _valeurD;
+            return resultat;
+        }
+
+        static double Soustraire(double _valeurG, double _valeurD)
+        {
+            double resultat = _valeurG - _valeurD;
+            return resultat;
+        }
+    }
+}
---
+++ Conception/03_Exos_CSharp/01_Expression/Expression/Expression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression
+{
+    internal class Expression
+    {
+        static double Calculer(double _valeurG, double _valeurD)
+        {
+          double result = _valeurG + _valeurD;
+          return result;
+        }
+    }
+}
